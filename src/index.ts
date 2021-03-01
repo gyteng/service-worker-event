bd378664046d714b19cf389767350d219eb77412
Gyt-OMEN <igyteng@gmail.com>
2021-03-01 19:17:16 +0800

    typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+declare global {
+  interface Window { swe: ClientEvent; }
+}
 import ClientEvent from './client-event';
 
 const initWorker = async () => {
-  const registration = await navigator.serviceWorker.register('./service-worker.bundle.js', { scope: '/' });
+  const registration = await navigator.serviceWorker.register('./service-worker.bundle.js', { scope: './' });
   let worker;
   if (registration.installing) {
     worker = registration.installing;
@@ -10,7 +13,6 @@ const initWorker = async () => {
   } else if (registration.active) {
     worker = registration.active;
     const swe = new ClientEvent(worker);
-    // @ts-ignore
     window.swe = swe;
     return;
   }
@@ -18,7 +20,6 @@ const initWorker = async () => {
     worker.addEventListener('statechange', function (e) {
       if (e.target.state === 'activated') {
         const swe = new ClientEvent(worker);
-        // @ts-ignore
         window.swe = swe;
       }
     });
