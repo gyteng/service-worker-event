4bc66716bad9b4be4343830805816c0bb7bdfc59
GYT-THINKPAD <igyteng@gmail.com>
2021-05-10 22:06:36 +0800

    add more test case

--- test/on.test.js
+++ test/on.test.js
@@ -65,4 +65,33 @@ describe('On event', () => {
     expect(text0).toBe('test_on1');
     expect(text1).toBe('test_on1');
   });
+
+  
+  it('on error', async () => {
+    await page0.evaluate(() => {
+      window.testOnError = [];
+      window.testOnError0 = data => {
+        testOnError[0] = data;
+      };
+      window.testOnError1 = data => {
+        testOnError[1] = data;
+        throw new Error('error');
+      };
+      window.testOnError2 = data => {
+        testOnError[2] = data;
+      };
+      swe.on('test_on_error', window.testOnError0);
+      swe.on('test_on_error', window.testOnError1);
+      swe.on('test_on_error', window.testOnError2);
+    });
+    await sleep(30);
+    await page1.evaluate(() => {
+      swe.emit('test_on_error', 'AsDfGh12345');
+    });
+    const [text0, text1, text2] = await page0.evaluate(() => {
+      return window.testOnError;
+    });
+    expect(text0).toBe('AsDfGh12345');
+    expect(text2).toBe('AsDfGh12345');
+  });
 });
